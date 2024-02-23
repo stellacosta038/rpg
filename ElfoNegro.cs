f879b832d25a49369569d34ddd1653e76763e783
stellacosta038 <139595986+stellacosta038@users.noreply.github.com>
2024-02-23 20:00:46 -0300

    Add files via upload

--- ElfoNegro.cs
+++ ElfoNegro.cs
@@ -1,6 +1,6 @@
 class ElfoNegro : Monstro
 {
-    public ElfoNegro() : base("Elfo Negro", 200, 3, 2, 100, 100) { }
+    public ElfoNegro() : base("Elfo Negro", 200, 3, 2, 100, 200) { }
 
     public override Monstro EncontrarMonstro()
     {
